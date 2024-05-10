01315d3cd99d71e9782dbd84d7376f834aeeb333
Alex <alex@blueselene.com>
2024-05-10 19:39:48 +0200

    Add Hooks.php

---
+++ includes/Hooks.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MediaWiki\Extension\HitCounters2;
+
+use MediaWiki\Output\Hook\BeforePageDisplayHook;
+use MediaWiki\Hook\GetMagicVariableIDsHook;
+
+class Hooks implements
+	BeforePageDisplayHook,
+	GetMagicVariableIDsHook
+{
+	public function onBeforePageDisplay( $out, $skin ): void {
+		$out->addModules( 'ext.hitcounters2.incrementcounter.js' );
+	}
+	public function getMagicVariableIDsHook( &$variableIDs ) {
+		$variableIDs[] = 'numberofhits';
+	}
+}
