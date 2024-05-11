6b2cd47f294b639496bed677220df729dc088a60
Alex <alex@blueselene.com>
2024-05-12 00:03:30 +0200

    Increment hit counter on every page view

--- modules/ext.hitcounters2.incrementcounter.js
+++ modules/ext.hitcounters2.incrementcounter.js
@@ -1,5 +1,6 @@
-( function () {
-	$( function () {
-		console.log(mw.rest);
-	} )
+$( function () {
+	var restApi = new mw.Rest();
+	restApi.post( '/hitcounters2/v0/increment/' + mw.config.get( 'wgArticleId' ), {
+		increment: true
+	} );
 } )
