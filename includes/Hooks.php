70e72de1578264b246cb8d655495293afb7a15c2
Alex <alex@blueselene.com>
2024-07-20 18:11:50 +0200

    Delete from table upon page deletion; cleanup

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,15 +2,21 @@
 
 namespace MediaWiki\Extension\HitCounters2;
 
-use MediaWiki\Output\Hook\BeforePageDisplayHook;
+use ManualLogEntry;
 use MediaWiki\Hook\GetMagicVariableIDsHook;
 use MediaWiki\Hook\ParserGetVariableValueSwitchHook;
+use MediaWiki\Output\Hook\BeforePageDisplayHook;
+use MediaWiki\Page\ProperPageIdentity;
+use MediaWiki\Page\Hook\PageDeleteCompleteHook;
+use MediaWiki\Permissions\Authority;
+use MediaWiki\Revision\RevisionRecord
 use Wikimedia\Rdbms\IConnectionProvider;
 
 class Hooks implements
 	BeforePageDisplayHook,
 	GetMagicVariableIDsHook,
-	ParserGetVariableValueSwitchHook
+	PageDeleteCompleteHook,
+	ParserGetVariableValueSwitchHook,
 {
 
 	private $connectionProvider;
@@ -36,7 +42,7 @@ class Hooks implements
 			$hitCount = $dbr->newSelectQueryBuilder()
 				->select( [ 'hc_hits' ] )
 				->from( 'hc2_hitcounter' )
-				->where( [ 'hc_pageid' => $pageID ])
+				->where( [ 'hc_pageid' => $pageID ] )
 				->caller( __METHOD__ )->fetchField();
 			if ( !$hitCount ) {
 				// Page has not yet been recorded in the database
@@ -44,6 +50,35 @@ class Hooks implements
 				return;
 			}
 			$ret = (int)$hitCount;
+		}
+	}
+
+	public function onPageDeleteComplete(
+		ProperPageIdentity $page,
+		Authority $deleter,
+		string $reason,
+		int $pageID,
+		RevisionRecord $deletedRev,
+		ManualLogEntry $logEntry,
+		int $archivedRevisionCount
+	) {
+		/*
+		 * Check if the page ID exists in the hc2_hitcounter table.
+		 * If so, delete it.
+		 */
+		$dbw = $this->connectionProvider->getPrimaryDatabase();
+		$pageExists = $dbw->newSelectQueryBuilder()
+			->select( [ 'hc_hits' ] )
+			->from( 'hc2_hitcounter' )
+			->where( [ 'hc_pageid' => $pageID ] )
+			->caller( __METHOD__ )->fetchField();
+		if ( !$pageExists ) {
+			// Nothing to do
+		} else {
+			$dbw->newDeleteQueryBuilder()
+				->deleteFrom( 'hc2_hitcounter' )
+				->where( [ 'hc_pageid' => $pageID ] )
+				->caller( __METHOD__ )->execute();
 		}
 	}
 }
