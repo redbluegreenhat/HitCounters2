bdc52745be25fb5d21acfbc79a1cd90c10c2c370
Alex <alex@blueselene.com>
2024-07-20 17:15:24 +0200

    Implement magic words

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -4,15 +4,36 @@ namespace MediaWiki\Extension\HitCounters2;
 
 use MediaWiki\Output\Hook\BeforePageDisplayHook;
 use MediaWiki\Hook\GetMagicVariableIDsHook;
+use MediaWiki\Hook\ParserGetVariableValueSwitchHook;
 
 class Hooks implements
 	BeforePageDisplayHook,
-	GetMagicVariableIDsHook
+	GetMagicVariableIDsHook,
+	ParserGetVariableValueSwitchHook
 {
 	public function onBeforePageDisplay( $out, $skin ): void {
 		$out->addModules( 'ext.hitcounters2.incrementcounter' );
 	}
+
 	public function onGetMagicVariableIDs( &$variableIDs ) {
 		$variableIDs[] = 'numberofhits';
+	}
+
+	public function onParserGetVariableSwitch( $parser, &$variableCache, $magicWordId, &$ret, $frame ) {
+		if ( $magicWordId === 'numberofhits' ) {
+			$dbr = $this->connectionProvider->getReplicaDatabase();
+			$pageID = $frame->getTitle()->getId();
+			$hitCount = $dbr->newSelectQueryBuilder()
+				->select( [ 'hc_hits' ] )
+				->from( 'hc2_hitcounter' )
+				->where( [ 'hc_pageid' => $pageID ])
+				->caller( __METHOD__ )->fetchField();
+			if ( !$hitCount ) {
+				// Page has not yet been recorded in the database
+				$ret = 0;
+				return;
+			}
+			$ret = (int)$hitCount;
+		}
 	}
 }
